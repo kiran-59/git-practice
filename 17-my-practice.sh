9852f1a1c7944e6377224122ebf478989106f349
kiran-59 <punaganikirankumar@gmail.com>
2024-10-06 12:52:05 +0530

    my practice.sh

--- 17-my-practice.sh
+++ 17-my-practice.sh
@@ -18,26 +18,27 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0]
     then
-    echo -e"$2 is.. $R FAILED $N"
+    echo -e "$2 is.. $R FAILED $N"
     exit1
-else
-  echo -e"$2 is... $R succes $N"
-fi
+    else
+    echo -e "$2 is... $R succes $N"
+    fi
 }
 
 
 CHECK_ROOT
 #sh 17-my-practice.sh git mysql postfix nginx
-for package in $@ refers to all arguments passed to it
+for package in $@ # $@ refers to all arguments passed to it
 
 do
-dnf list installed $package
-if [ $? -ne 0]
-then
-echo"$package is not installed, going to install it.."
-dnf install $package -y
-VALIDATE $? "installing $package"
-else
-echo"$package is already installed.. nothing to do"
-fi
+  dnf list installed $package
+  if [ $? -ne 0 ]
+  then
+  echo "$package is not installed, going to install it.."
+  dnf install $package -y
+  VALIDATE $? "installing $package"
+
+  else
+      echo "$package is already installed.. nothing to do"
+      fi
 done
